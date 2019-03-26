a4cbf5ac2ca0154112981dadedc0cfa8722a85a9
Sylene Manusset <sylene.manusset@gmail.com>
2019-03-26 14:44:28 +0100

    dojo of Cloe and Sylene

---
+++ test.js
@@ -0,0 +1,9 @@
+const assert = require("assert");
+const compSquare = require("./function");
+
+assert.strictEqual(typeof compSquare, "function");
+assert.strictEqual(compSquare.isArray(array1), true);
+assert.strictEqual(Array.isArray(array2), true);
+assert.strictEqual(compSquare(array1, arra2), true);
+
+console.log("test OK");
